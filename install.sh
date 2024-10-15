7b0aad3dd0f3c7497fc96b3b433187e47f00d2f6
Whispering Wind <151555003+ReturnFI@users.noreply.github.com>
2024-10-15 21:31:24 +0330

    Better installation

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if sudo -n true 2>/dev/null; then
-    echo "This User has sudo permissions"
+    echo "This user has sudo permissions"
 else
-    echo "This User does not have sudo permissions"
+    echo "This user does not have sudo permissions"
     exit 1
 fi
 
@@ -11,33 +11,23 @@ if [ -f /etc/redhat-release ]; then
     if grep -q "Rocky" /etc/redhat-release; then
         OS="Rocky"
         PACKAGE_MANAGER="dnf"
-        SERVICE_MANAGER="systemctl"
     elif grep -q "AlmaLinux" /etc/redhat-release; then
         OS="AlmaLinux"
         PACKAGE_MANAGER="dnf"
-        SERVICE_MANAGER="systemctl"
     else
         OS="CentOS"
         PACKAGE_MANAGER="yum"
-        SERVICE_MANAGER="systemctl"
     fi
 elif [ -f /etc/os-release ]; then
     . /etc/os-release
     case "$ID" in
-        ubuntu)
-            OS="Ubuntu"
+        ubuntu|debian)
+            OS="Debian/Ubuntu"
             PACKAGE_MANAGER="apt"
-            SERVICE_MANAGER="systemctl"
-            ;;
-        debian)
-            OS="Debian"
-            PACKAGE_MANAGER="apt"
-            SERVICE_MANAGER="systemctl"
             ;;
         fedora)
             OS="Fedora"
             PACKAGE_MANAGER="dnf"
-            SERVICE_MANAGER="systemctl"
             ;;
         *)
             echo "Unsupported OS"
@@ -50,21 +40,29 @@ else
 fi
 
 if [ "$PACKAGE_MANAGER" = "apt" ]; then
-    sudo apt update -qq
-    echo "Update Server"
+    sudo apt update -qq > /dev/null 2>&1 && echo "Server updated"
 else
-    sudo $PACKAGE_MANAGER update -y -q
-    echo "Update Server"
+    sudo $PACKAGE_MANAGER update -y -q > /dev/null 2>&1 && echo "Server updated"
 fi
+
+necessary_packages=(
+    dialog
+    whiptail
+    jq
+    lsof
+    tar
+    wget
+    git
+)
 
 install_package() {
     package=$1
     if ! command -v $package &> /dev/null; then
-        echo -n "Installing $package..."
-        if sudo $PACKAGE_MANAGER install $package -y -qq; then
-            echo -e "✓"
+        echo -n "Installing $package... "
+        if sudo $PACKAGE_MANAGER install $package -y -qq > /dev/null 2>&1; then
+            echo "✓"
         else
-            echo -e " ✕"
+            echo "✕"
             exit 1
         fi
     else
@@ -72,14 +70,12 @@ install_package() {
     fi
 }
 
-install_package dialog
-install_package whiptail
-install_package jq
-install_package lsof
-install_package tar 
-install_package wget
-install_package git
+# Install all necessary packages
+for package in "${necessary_packages[@]}"; do
+    install_package "$package"
+done
 
+# Add alias to .bashrc if not already present
 if ! grep -q "alias relay='bash -c \"/opt/hiddify-relay/menu.sh\"'" ~/.bashrc; then
     echo "alias relay='bash -c \"/opt/hiddify-relay/menu.sh\"'" >> ~/.bashrc
     echo "Alias added to .bashrc"
@@ -89,12 +85,10 @@ else
     source ~/.bashrc
 fi
 
-sleep 5
+sleep 3
 clear
 
-
-git clone -b beta https://github.com/hiddify/hiddify-relay  /opt/hiddify-relay
-
+git clone -b beta https://github.com/hiddify/hiddify-relay /opt/hiddify-relay > /dev/null 2>&1
 cd /opt/hiddify-relay
 chmod +x menu.sh
 ./menu.sh
